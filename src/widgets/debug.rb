63fe784ca5c6c5ee8d94073d4504fa8b86504a3d
LevitatingBusinessMan (Rein Fernhout) <me@levitati.ng>
2025-05-05 21:26:00 +0200

    make debug widget only work when interactive

--- src/widgets/debug.rb
+++ src/widgets/debug.rb
@@ -5,11 +5,10 @@ require_relative "../widgets"
 # Break the program and jump into debugger
 class Widgets::Debug < Widgets::Widget
     def initialize options
-        super        
+        super
         @button = Gtk::Button.new label: 'bye 🐛'
         @button.signal_connect("clicked") do
-            return unless $stdout.isatty # doesn't work
-            binding.break
+            binding.break if $stdout.isatty and $stdin.isatty
         end
         append @button
     end
